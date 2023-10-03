08f44e684e5201043645c9c55a1ea344c6e6e453
Akley Alves <akleyalvescr@gmail.com>
2023-10-02 22:00:20 -0300

    Update arrays.js

--- logica-de-programacao/arrays/arrays.js
+++ logica-de-programacao/arrays/arrays.js
@@ -1,5 +1,4 @@
 var segredos = ["Akley", 13, true, {teste: 1, teste: 2, teste: 3}]; // dentro de {} são objetos
-console.log(segredos);
 
 // mostrando o elemento que está em cada posição no console do navegador.
 console.log(segredos[0]);
@@ -7,12 +6,14 @@ console.log(segredos[1]);
 console.log(segredos[2]);
 console.log(segredos[3]);
 
-// Outra maneira de adicionar elemento no array
+
+// adicionar elemento no array
 segredos[4] = "Karinne";
-console.log(segredos);
 console.log(segredos[4]);
 
 //substituindo valor do array
 segredos[0] = "Juan";
 console.log(segredos);
 
+// Outra maneira de Adicionar elemento em array
+segredos.push(999);
